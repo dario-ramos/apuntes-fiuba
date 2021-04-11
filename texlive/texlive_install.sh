87e4d20623ca705ad94eb8a41cc2d6ed32edfa2b
dario-ramos <dario.fenix@gmail.com>
2021-04-11 16:17:01 -0300

    Momentarily disable caching to get latest TexLive

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -5,18 +5,18 @@
 # A minimal current TL is installed adding only the packages that are required
 
 # See if there is a cached version of TL available
-export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
-if ! command -v texlua > /dev/null; then
-  # Obtain TeX Live
-  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-  tar -xzf install-tl-unx.tar.gz
-  cd install-tl-20*
+# export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+# if ! command -v texlua > /dev/null; then
+# Obtain TeX Live
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar -xzf install-tl-unx.tar.gz
+cd install-tl-20*
 
-  # Install a minimal system
-  ./install-tl --profile=../texlive/texlive.profile
+# Install a minimal system
+./install-tl --profile=../texlive/texlive.profile
 
-  cd ..
-fi
+cd ..
+# fi
 
 # Sometimes tlmgr needs to be updated before we can install packages
 tlmgr update --self
